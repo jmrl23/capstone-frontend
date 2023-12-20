e15d2ffda3d78cdcc4637fe08e0ba430db4796bc
Jomariel Gaitera <jojogaitera@gmail.com>
2023-12-20 09:26:33 +0800

    Add PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { default as react } from '@vitejs/plugin-react-swc';
 import { join } from 'node:path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -18,11 +19,46 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
-    plugins: [react()],
+    plugins: [
+      VitePWA({
+        manifestFilename: 'manifest.json',
+        registerType: 'autoUpdate',
+        minify: true,
+        workbox: {
+          sourcemap: true,
+        },
+        devOptions: {
+          enabled: true,
+        },
+        manifest: {
+          name: 'IoT Inhaler Alert System',
+          short_name: 'IoT Inhaler',
+          description: 'Capstone project',
+          theme_color: '#ffffff',
+          orientation: 'portrait',
+          icons: [
+            {
+              src: 'assets/image/icon-192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'assets/image/icon-512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+          ],
+        },
+      }),
+      react(),
+    ],
     resolve: {
       alias: {
         '@': join(__dirname, './src'),
       },
+    },
+    build: {
+      chunkSizeWarningLimit: 900,
     },
   };
 });
