96520d379f446c4fe7b258b05b1893b904d756c0
Jomariel Gaitera <jojogaitera@gmail.com>
2023-12-20 20:23:21 +0800

    Set hax storage

--- src/hooks/useHax.ts
+++ src/hooks/useHax.ts
@@ -1,17 +1,26 @@
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
-export const useHax = create<AuthorizationProps>()((set) => ({
-  value: {},
-  setValue: (cid) =>
-    set({
-      value: {
-        Authorization: `Bearer ${cid}`,
-      },
+export const useHax = create<HaxProps>()(
+  persist(
+    (set) => ({
+      value: {},
+      setValue: (cid) =>
+        set({
+          value: {
+            Authorization: `Bearer ${cid}`,
+          },
+        }),
+      reset: () => set({ value: {} }),
     }),
-  reset: () => set({ value: {} }),
-}));
+    {
+      name: 'hax',
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
+);
 
-export interface AuthorizationProps {
+export interface HaxProps {
   value:
     | {
         Authorization: string;
